bd2adce253403317e7e5211c2b9235615867e2e1
PathiVinayKumar <vinny.pathi@gmail.com>
2026-04-23 18:27:25 +0530

    commiting the changes

--- 03-frontend.sh
+++ 03-frontend.sh
@@ -1,20 +1,34 @@
 dnf install -y nginx
-echo -e "\e[23m >>>>>>>> Install Nginx <<<<<<<<<<< \e[0m"
+echo -e "\e[25m >>>>>>>> Install Nginx <<<<<<<<<<< \e[0m"
 
 cp nginx.conf /etc/nginx/nginx.conf
 
+echo -e "\e[25m >>>>>>>> Copy Nginx <<<<<<<<<<< \e[0m"
+
+
 curl -fsSL https://rpm.nodesource.com/setup_20.x | bash -
 dnf install -y nodejs
+echo -e "\e[25m >>>>>>>> Install Node js <<<<<<<<<<< \e[0m"
+
 
 curl -L -o /tmp/frontend.zip https://raw.githubusercontent.com/raghudevopsb89/roboshop-microservices/main/artifacts/frontend.zip
 rm -rf /tmp/frontend
 mkdir -p /tmp/frontend
 cd /tmp/frontend
+echo -e "\e[25m >>>>>>>> extracting the code and installing Nginx <<<<<<<<<<< \e[0m"
+
 unzip /tmp/frontend.zip
 npm install
+
+echo -e "\e[25m >>>>>>>> Installing frontend <<<<<<<<<<< \e[0m"
+
 npm run build
 rm -rf /usr/share/nginx/html/*
 cp -r out/* /usr/share/nginx/html/
 
+
+
 systemctl restart nginx
 systemctl enable nginx
+
+echo -e "\e[25m >>>>>>>> Restarting the Nginx <<<<<<<<<<< \e[0m"

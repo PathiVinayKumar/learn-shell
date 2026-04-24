04a008661f1d0048bedce67dd098225dfc4bef8e
PathiVinayKumar <vinny.pathi@gmail.com>
2026-04-24 20:32:36 +0530

    changes

--- 03-frontend.sh
+++ 03-frontend.sh
@@ -1,35 +1,30 @@
 echo -e "\e[35m >>>>>>>> Install Nginx <<<<<<<<<<< \e[0m"
-
 dnf install -y nginx &>>/tmp/nginx-output  &>>/tmp/roboshop.log
 
+echo -e "\e[35m >>>>>>>> Copy Nginx <<<<<<<<<<< \e[0m"
 cp nginx.conf /etc/nginx/nginx.conf &>>/tmp/nginx-output-conf
 
-echo -e "\e[35m >>>>>>>> Copy Nginx <<<<<<<<<<< \e[0m"
 
-
-curl -fsSL https://rpm.nodesource.com/setup_20.x | bash -
-dnf install -y nodejs &>>/tmp/nodejs-output  &>>/tmp/roboshop.log
 echo -e "\e[35m >>>>>>>> Install Node js <<<<<<<<<<< \e[0m"
+curl -fsSL https://rpm.nodesource.com/setup_20.x | bash -
+dnf install -y nodejs  &>>/tmp/roboshop.log
 
-
+echo -e "\e[35m >>>>>>>> extracting the code and installing Nginx <<<<<<<<<<< \e[0m"
 curl -L -o /tmp/frontend.zip https://raw.githubusercontent.com/raghudevopsb89/roboshop-microservices/main/artifacts/frontend.zip &>/tmp/null
 rm -rf /tmp/frontend 
 mkdir -p /tmp/frontend
 cd /tmp/frontend
-echo -e "\e[35m >>>>>>>> extracting the code and installing Nginx <<<<<<<<<<< \e[0m"
-
-unzip /tmp/frontend.zip
-npm install &>/tmp/null &>>/tmp/roboshop.log
 
 echo -e "\e[35m >>>>>>>> Installing frontend <<<<<<<<<<< \e[0m"
+unzip /tmp/frontend.zip
+npm install &>>/tmp/roboshop.log
 
-npm run build &>/tmp/null
+
+npm run build 
 rm -rf /usr/share/nginx/html/*
 cp -r out/* /usr/share/nginx/html/
 
-
-
+echo -e "\e[35m >>>>>>>> Restarting the Nginx <<<<<<<<<<< \e[0m"
 systemctl restart nginx
 systemctl enable nginx
 
-echo -e "\e[35m >>>>>>>> Restarting the Nginx <<<<<<<<<<< \e[0m"

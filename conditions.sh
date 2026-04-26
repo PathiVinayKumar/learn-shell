b50f3297b6f4f6af4fd6f8eca5526abe8aac8691
PathiVinayKumar <vinny.pathi@gmail.com>
2026-04-26 07:18:16 +0530

    added more conditions

--- conditions.sh
+++ conditions.sh
@@ -9,8 +9,14 @@ fi
 
 if [ $x -gt 200 ]; then
 echo x is greater than 200
+else
+echo x is not greater than 200
 fi
 
 if [ $x -lt 100 ]; then 
 echo x is lesser than 100
+else if [ $x -gt 100 ]; then 
+echo x is greater than 100
+else
+echo give value is not greater/lesser than 100
 fi
